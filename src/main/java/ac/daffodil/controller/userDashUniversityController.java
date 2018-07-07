50ac3e888f0009da63a3204488636c0fe51de59f
Muiduzzaman Lipu <mzlipu16@gmail.com>
2018-07-07 12:51:10 +0600

    solve dropdown menu problem...

--- src/main/java/ac/daffodil/controller/userDashUniversityController.java
+++ src/main/java/ac/daffodil/controller/userDashUniversityController.java
@@ -3,6 +3,7 @@ package ac.daffodil.controller;
 import ac.daffodil.dao.DepartmentDao;
 import ac.daffodil.dao.UniversityDao;
 import ac.daffodil.model.Department;
+import ac.daffodil.model.University;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
@@ -11,6 +12,7 @@ import org.springframework.web.servlet.ModelAndView;
 import javax.servlet.http.HttpServletRequest;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Optional;
 
 /**
  * Created by Muiduzzaman Lipu on 04-Apr-18.
@@ -48,6 +50,15 @@ public class userDashUniversityController {
 
         modelAndView.addObject("departmentList", departments1);
         modelAndView.setViewName("user/userDashDepartment");
+        return modelAndView;
+    }
+
+    @RequestMapping(value={"/findForUniversity/{id}"}, method = RequestMethod.GET)
+    public ModelAndView findForUniversity(@PathVariable(required = true, name = "id") Long id) {
+        ModelAndView modelAndView = new ModelAndView();
+        Optional<University> university= universityDao.find(id);
+        modelAndView.addObject("university", university.get());
+        modelAndView.setViewName("user/userDashUniversityDetails");
         return modelAndView;
     }
 }
--- src/main/java/ac/daffodil/model/University.java
+++ src/main/java/ac/daffodil/model/University.java
@@ -22,13 +22,37 @@ public class University {
     @Column(name="location")
     private String location;
 
+    @Column(name="total_student")
+    private Long totalStudent;
+
+    @Column(name="about")
+    private String about;
+
+    @Column(name="academic_staff")
+    private Long academicStaff;
+
+    @Column(name="administrative_staff")
+    private Long administrativeStaff;
+
+    @Column(name="undergraduates")
+    private Long undergraduates;
+
+    @Column(name="postgraduates")
+    private Long postgraduates;
+
     public University() {
     }
 
-    public University(String universityName, String status, String location) {
+    public University(String universityName, String status, String location, Long totalStudent, String about, Long academicStaff, Long administrativeStaff, Long undergraduates, Long postgraduates) {
         this.universityName = universityName;
         this.status = status;
         this.location = location;
+        this.totalStudent = totalStudent;
+        this.about = about;
+        this.academicStaff = academicStaff;
+        this.administrativeStaff = administrativeStaff;
+        this.undergraduates = undergraduates;
+        this.postgraduates = postgraduates;
     }
 
     public Long getUniversityId() {
@@ -63,6 +87,54 @@ public class University {
         this.location = location;
     }
 
+    public Long getTotalStudent() {
+        return totalStudent;
+    }
+
+    public void setTotalStudent(Long totalStudent) {
+        this.totalStudent = totalStudent;
+    }
+
+    public String getAbout() {
+        return about;
+    }
+
+    public void setAbout(String about) {
+        this.about = about;
+    }
+
+    public Long getAcademicStaff() {
+        return academicStaff;
+    }
+
+    public void setAcademicStaff(Long academicStaff) {
+        this.academicStaff = academicStaff;
+    }
+
+    public Long getAdministrativeStaff() {
+        return administrativeStaff;
+    }
+
+    public void setAdministrativeStaff(Long administrativeStaff) {
+        this.administrativeStaff = administrativeStaff;
+    }
+
+    public Long getUndergraduates() {
+        return undergraduates;
+    }
+
+    public void setUndergraduates(Long undergraduates) {
+        this.undergraduates = undergraduates;
+    }
+
+    public Long getPostgraduates() {
+        return postgraduates;
+    }
+
+    public void setPostgraduates(Long postgraduates) {
+        this.postgraduates = postgraduates;
+    }
+
     @Override
     public String toString() {
         return "University{" +
@@ -70,6 +142,12 @@ public class University {
                 ", universityName='" + universityName + '\'' +
                 ", status='" + status + '\'' +
                 ", location='" + location + '\'' +
+                ", totalStudent=" + totalStudent +
+                ", about='" + about + '\'' +
+                ", academicStaff=" + academicStaff +
+                ", administrativeStaff=" + administrativeStaff +
+                ", undergraduates=" + undergraduates +
+                ", postgraduates=" + postgraduates +
                 '}';
     }
 }
